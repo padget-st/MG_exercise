4f357d46fdddb1ca35fb70bfbdd7340cd6b4a359
Roman Romanteev <romanteevr@dc.psa.inc>
2021-12-16 17:42:21 +0300

    working on chapter12_2

--- chapter_12_2/Chapter12_2/Chapter12_2.cpp
+++ chapter_12_2/Chapter12_2/Chapter12_2.cpp
@@ -4,8 +4,17 @@
 int main()
 {
     
-    Grid<int> grid_int;
+    Grid<> grid_int;
     grid_int.at(2, 2) = 8;
-    std::cout << grid_int.at(2, 2).value_or(0);
+    grid_int.setData(13);
+    std::cout << grid_int.at(2, 2).value_or(0.0) << " " << grid_int.getData() << '\n';
+
+    Grid<double, 11, 11> grid_double;
+    grid_double.setData(4.3);
+    grid_double.at(2, 2) = 4;
+    std::cout << grid_double.at(2, 2).value_or(0.0) << " " << grid_double.getData() << '\n';
+
+    grid_double = grid_int;
+    std::cout << grid_double.at(2, 2).value_or(0.0) << " " << grid_double.getData();
 }
 
--- chapter_12_2/Chapter12_2/Grid.h
+++ chapter_12_2/Chapter12_2/Grid.h
@@ -3,64 +3,112 @@
 #include <optional>
 #include <format>
 
-template <typename T>
+constexpr size_t DEFAULT_WIDTH{ 10 };
+constexpr size_t DEFAULT_HEIGHT{ 10 };
+
+constexpr size_t getDefaultWidth()
+{
+	return DEFAULT_WIDTH * 2;
+}
+
+constexpr size_t getDefaultHeight()
+{
+	return DEFAULT_HEIGHT * 2;
+}
+
+template <typename T = int,
+	size_t WIDTH = getDefaultWidth(), size_t HEIGHT = getDefaultHeight()>
 class Grid
 {
 public:
-	explicit Grid(size_t width = DefaultWidth, size_t height = DefaultHeight);
+	Grid() = default;
 	virtual ~Grid() = default;
 
 	Grid(const Grid& src) = default;
 	Grid& operator=(const Grid& rhs) = default;
 
-	Grid(Grid&& src) = default;
-	Grid& operator=(Grid&& src) = default;
+	template <typename E, size_t WIDTH2, size_t HEIGHT2>
+	Grid(const Grid<E, WIDTH2, HEIGHT2>& src);
+
+	template <typename E, size_t WIDTH2, size_t HEIGHT2>
+	Grid& operator=(const Grid<E, WIDTH2, HEIGHT2>& rhs);
 	
+	void swap(Grid& other) noexcept;
+
 	std::optional<T>& at(size_t x, size_t y);
 	const std::optional<T>& at(size_t x, size_t y) const;
 
-	static const size_t DefaultWidth{ 10 };
-	static const size_t DefaultHeight{ 10 };
+	size_t getWidth() const { return WIDTH; };
+	size_t getHeight() const { return HEIGHT; };
+
+	void setData(T data) { m_data = data; }
+	T getData() const { return m_data;  }
 private:
 	void verifyCoordinate(size_t x, size_t y) const;
 
-	std::vector<std::vector<std::optional<T>>> m_cells;
-	size_t m_width{}, m_height{};
+	std::optional<T> m_cells[WIDTH][HEIGHT];
+	T m_data{};
 };
 
-template <typename T>
-Grid<T>::Grid(size_t width, size_t height)
-	: m_width{ width }, m_height{ height }
+template <typename T, size_t WIDTH, size_t HEIGHT>
+template <typename E, size_t WIDTH2, size_t HEIGHT2>
+Grid<T, WIDTH, HEIGHT>::Grid(const Grid<E, WIDTH2, HEIGHT2>& src)
+	: m_data{ static_cast<T>(src.getData()) }
 {
-	m_cells.resize(width);
-	for (auto& elem : m_cells)
+	for (size_t i{}; i < WIDTH; i++)
 	{
-		elem.resize(height);
+		for (size_t j{}; j < HEIGHT; j++)
+		{
+			if (i < WIDTH2 && j < HEIGHT2)
+			{
+				m_cells[i][j] = src.at(i, j);
+			}
+			else
+			{
+				m_cells[i][j].reset();
+			}
+		}
 	}
 }
 
-template <typename T>
-void Grid<T>::verifyCoordinate(size_t x, size_t y) const
+template <typename T, size_t WIDTH, size_t HEIGHT>
+void Grid<T, WIDTH, HEIGHT>::swap(Grid& other) noexcept
 {
-	if (x >= m_width)
+	std::swap(m_cells, other.m_cells);
+	std::swap(m_data, other.m_data);
+}
+
+template <typename T, size_t WIDTH, size_t HEIGHT>
+template <typename E, size_t WIDTH2, size_t HEIGHT2>
+Grid<T, WIDTH, HEIGHT>& Grid<T, WIDTH, HEIGHT>::operator=(const Grid<E, WIDTH2, HEIGHT2>& rhs)
+{
+	Grid<T, WIDTH, HEIGHT> temp{ rhs };
+	swap(temp);
+	return *this;
+}
+
+template <typename T, size_t WIDTH, size_t HEIGHT>
+void Grid<T, WIDTH, HEIGHT>::verifyCoordinate(size_t x, size_t y) const
+{
+	if (x >= WIDTH)
 	{
-		throw std::runtime_error(std::format("{} must be less than {}", x, m_width));
+		throw std::runtime_error(std::format("{} must be less than {}", x, WIDTH));
 	}
-	if (y >= m_height)
+	if (y >= HEIGHT)
 	{
-		throw std::runtime_error(std::format("{} must be less than {}", y, m_height));
+		throw std::runtime_error(std::format("{} must be less than {}", y, HEIGHT));
 	}
 }
 
-template <typename T>
-const std::optional<T>& Grid<T>::at(size_t x, size_t y) const
+template <typename T, size_t WIDTH, size_t HEIGHT>
+const std::optional<T>& Grid<T, WIDTH, HEIGHT>::at(size_t x, size_t y) const
 {
 	verifyCoordinate(x, y);
 	return m_cells[x][y];
 }
 
-template <typename T>
-std::optional<T>& Grid<T>::at(size_t x, size_t y)
+template <typename T, size_t WIDTH, size_t HEIGHT>
+std::optional<T>& Grid<T, WIDTH, HEIGHT>::at(size_t x, size_t y)
 {
 	return const_cast<std::optional<T>&>(std::as_const(*this).at(x, y));
 }
